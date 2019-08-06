73880c01ef92506629fc6a5639e16ac56c1f37fc
Sargy <lukina_sargy@mail.ru>
2019-08-06 18:49:26 +0300

    paddle added

--- app/js/breackout.js
+++ app/js/breackout.js
@@ -1,43 +1,119 @@
 function breackout() {
+  //defining canvas
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
-  //  ctx.beginPath();
-  //  ctx.rect(20, 40, 50, 50);
-  //  ctx.fillStyle = 'red';
-  //  ctx.fill();
-  //  ctx.closePath();
-  //
-  //  ctx.beginPath();
-  //  ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-  //  ctx.fillStyle = "green";
-  //  ctx.fill();
-  //  ctx.closePath();
-  //
-  //  ctx.beginPath();
-  //  ctx.rect(160, 10, 100, 40);
-  //  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-  //  ctx.stroke();
-  //  ctx.closePath();
-  var x = 0; //begining coordinates
-  var y = canvas.height/2; //begining coordinates
-  var dx = 0.2;
-//  var dy = Math.sqrt(2500 - x*x);
+  //variables start
+
+  var ballRadius = 20;
+  var x = ballRadius; //begining coordinates
+  var y = canvas.height / 2; //begining coordinates
+  var dx = 4;
+  var dy = -4;
+  var ballColor = "yellow";
+
+
+  //paddle parameters
+  var paddleHeight = 20;
+  var paddleWidth = 140;
+  var paddleColor = 'grey';
+  var paddleX = (canvas.width - paddleWidth) / 2;
+  var rightPressed = false;
+  var leftPressed = false;
+
+  //variables end
+
+  //event listeners for paddle start
+
+  document.addEventListener('keydown', keyDownHandler, false);
+  document.addEventListener('keyup', keyUpHandler, false);
+
+  function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+  //event listeners for paddle end
+
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
   function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI * 2);
-    ctx.fillStyle = "yellow";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
   }
 
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = paddleColor;
+    ctx.fill();
+    ctx.closePath();
+  }
+  function gameOver() {
+    ctx.fillStyle = "#F00";
+    ctx.font = 'bold 40px sans-serif';
+    ctx.fillText("Game Over", (canvas.width/2 - 100), canvas.height/2);
+    clearInterval(refreshIntervalId);
+  }
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
+
     x += dx;
-    y += Math.cos(x/10);
+    y += dy;
+    if (y - ballRadius < 0 ) {
+      dy = -dy;
+      ballColor = getRandomColor();
+      drawBall();
+    }
+    if (y - ballRadius > canvas.height) {
+      gameOver();
+      
+    }
+    if (x - ballRadius < 0 || x + ballRadius > canvas.width  ) {
+      dx = -dx;
+      ballColor = getRandomColor();
+      drawBall();
+    }
+    if ( ( paddleX < x && x < (paddleX + paddleWidth) ) && (y + ballRadius >  canvas.height - paddleHeight) ) {
+      dy = -dy;
+      ballColor = getRandomColor();
+      drawBall();
+    }
+
+    //paddle moving logic
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+      paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+//    $('#key').html('right=' + rightPressed + ' left =' + leftPressed);
+
   }
-  setInterval(draw, 10);
+  
+  var refreshIntervalId = setInterval(draw, 10);
 
 }
